f47f5a10cbd9b860929f751d0a8eda1a5e6a4ef6
Elang Argawana <elangargawana.15@gmail.com>
2026-03-30 22:28:34 +0700

    fix: build

--- src/components/landing/about/LocationSection.vue
+++ src/components/landing/about/LocationSection.vue
@@ -71,7 +71,7 @@
           v-animateonscroll="{ enterClass: 'animate-scalein', leaveClass: 'animate-fadeout' }"
         >
           <!-- Embedded Google Map for interactive location -->
-          <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1979.956584062852!2d110.33500179073792!3d-7.019492570567144!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2e708b001478d139%3A0xf40ddc5c17cc4434!2sARLETTA%20GYM!5e0!3m2!1sen!2sid!4v1774884111212!5m2!1sen!2sid" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+          <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1979.956584062852!2d110.33500179073792!3d-7.019492570567144!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2e708b001478d139%3A0xf40ddc5c17cc4434!2sARLETTA%20GYM!5e0!3m2!1sen!2sid!4v1774884111212!5m2!1sen!2sid" width="600" height="450" style="border:0;" :allowfullscreen="true" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
         </div>
       </div>
     </div>
